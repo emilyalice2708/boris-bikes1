60214265d6523f43db37abbe16ad39942db76141
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-01 22:31:38 +0100

    Step 12 completed. DockingStation objects can now track docked bikes and only release when bikes are available. Tests passing.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,33 @@
 require 'docking_station'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+  it 'releases bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
+  end
 
   it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it 'raises error if no bikes docked' do
+    expect { subject.release_bike }.to raise_error 'Error: no bikes docked.'
+  end
+
+  it 'docks bikes' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it { is_expected.to respond_to(:bike)}
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
